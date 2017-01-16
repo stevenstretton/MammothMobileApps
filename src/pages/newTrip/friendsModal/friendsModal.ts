ab97bc4f26c6aeb78042705378bee2ba48ded604
Will Ashworth <wmashworth@hotmail.com>
2017-01-16 09:14:35 +0000

    Added comments and formatted code

--- src/pages/newTrip/friendsModal/friendsModal.ts
+++ src/pages/newTrip/friendsModal/friendsModal.ts
@@ -3,8 +3,8 @@ import { FirebaseGET } from '../../../services/firebaseGET.service';
 import { ViewController, Platform, NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-friendsModal',
-  templateUrl: 'friendsModal.html'
+	selector: 'page-friendsModal',
+	templateUrl: 'friendsModal.html'
 })
 export class FriendsModal {
 	private _friends: Array<any>;
@@ -13,54 +13,63 @@ export class FriendsModal {
 	private _unchangedFriends: Array<any>;
 
 	constructor(public viewCtrl: ViewController,
-				public firebaseGet: FirebaseGET,
-				public params: NavParams,
-        ){
+	            public firebaseGet: FirebaseGET,
+	            public params: NavParams,) {
 		this._friends = [];
 		this._selectedFriends = params.get('selectedFriends');
-		this._unchangedFriends =  params.get('selectedFriends');
+		this._unchangedFriends = params.get('selectedFriends');
 		console.log("this._selectedFriends");
 		console.log(this._selectedFriends);
 		this._currentUser = params.get('currentUser');
 
+		// Okay so this adds all users to another array but adds a flag as to whether they have been previously added in another model
 		this._currentUser.friends.forEach((friendID) => {
+
+			// Actually, this whole section can be refactored into:
+			//
+			// if (this._selectedFriends.indexOf(friendID) > -1) {
+			//     checked = true;
+			// } else {
+			//     checked = false;
+			// }
 			this.firebaseGet.getUserWithID(friendID, (friend) => {
 				let checked = false;
-				for(let i = 0; i < this._selectedFriends.length; ++i)
-				{
-					if (friend.key == this._selectedFriends[i])
-					{
+				// Can redo this 'for' into a 'forEach'
+				for (let i = 0; i < this._selectedFriends.length; ++i) {
+					if (friend.key == this._selectedFriends[i]) {
 						checked = true;
 					}
 				}
-				this._friends.push( {friend: friend, checked: checked});
-			});		
+				this._friends.push({friend: friend, checked: checked});
+			});
 		});
 		console.log("this._friends");
 		console.log(this._friends);
 	}
 
-  
-  dismiss() {
-    this.viewCtrl.dismiss(this._unchangedFriends);
-  }
+	// What's the difference between dismiss and submit here?
+	dismiss() {
+		this.viewCtrl.dismiss(this._unchangedFriends);
+	}
 
 	submit() {
 		this.viewCtrl.dismiss(this._selectedFriends);
 	}
 
+	// There is a better way to do this:
+	//
+	// return (this._selectedFriends.indexOf(friend) > -1);
 	ifInArray(friend): boolean {
 		let inArray = false
-		
+
 		this._selectedFriends.forEach(oldFriend => {
-			if (oldFriend == friend.key)
-			{
+			if (oldFriend == friend.key) {
 				inArray = true;
-				
+
 			}
 		});
 		console.log(inArray);
-		
+
 		//console.log(this._selectedFriends.indexOf(friend));
 		//console.log(this._selectedFriends);
 		//return (this._selectedFriends.indexOf(friend) > -1);
@@ -73,7 +82,7 @@ export class FriendsModal {
 				this._selectedFriends.push(friend.friend.key);
 				//console.log(this._selectedFriends);
 			}
-			
+
 		} else {
 			if (this.ifInArray(friend.friend)) {
 				this._selectedFriends.splice(this._selectedFriends.indexOf(friend.friend.key), 1);
--- src/pages/newTrip/newTrip.ts
+++ src/pages/newTrip/newTrip.ts
@@ -2,192 +2,187 @@ import { Component } from '@angular/core';
 import { FirebaseGET } from '../../services/firebaseGET.service';
 import { FirebasePUSH } from '../../services/firebasePUSH.service';
 import { AuthenticationHandler } from '../../services/authenticationHandler.service';
-import { NavController, ActionSheetController, Platform, ModalController, ToastController} from 'ionic-angular';
-import { FriendsModal } from '../newTrip/friendsModal/friendsModal';
+import { NavController, ActionSheetController, Platform, ModalController, ToastController } from 'ionic-angular';
+import { FriendsModal } from './friendsModal/friendsModal';
 
 @Component({
-  selector: 'page-newTrip',
-  templateUrl: 'newTrip.html',
+	selector: 'page-newTrip',
+	templateUrl: 'newTrip.html',
 })
 export class NewTrip {
-  public _friendsAdded: Array<any>;
-  private _currentUser: any;
-  private _tripInfo: any;
-  private _itemList: Array<any>;
-  title;
-  descr;
-  tripName;
-  tripLoc;
-  tripDescription;
 
+	// These can all be private
+	public _friendsAdded: Array<any>;
+	private _currentUser: any;
+	private _tripInfo: any;
+	private _itemList: Array<any>;
+	title;
+	descr;
+	tripName;
+	tripLoc;
+	tripDescription;
 
+	constructor(public navCtrl: NavController,
+	            public actionSheetCtrl: ActionSheetController,
+	            public platform: Platform,
+	            public firebaseGet: FirebaseGET,
+	            public authenticationHandler: AuthenticationHandler,
+	            public modalCtrl: ModalController,
+	            public firebasePush: FirebasePUSH,
+	            private toastCtrl: ToastController) {
 
-  constructor(
-    public navCtrl: NavController, 
-    public actionSheetCtrl: ActionSheetController,
-    public platform: Platform,
-    public firebaseGet: FirebaseGET,
-		public authenticationHandler: AuthenticationHandler,
-		public modalCtrl: ModalController,
-		public firebasePush: FirebasePUSH,
-    private toastCtrl: ToastController
-    ) {
-
-    this._friendsAdded = [];
+		this._friendsAdded = [];
 		this._currentUser = this.authenticationHandler.getCurrentUser();
-    this._itemList = [];
+		this._itemList = [];
 
-    this._tripInfo;
+		// This doesn't need to be here
+		this._tripInfo;
 
-      
-    }
+	}
 
-  public event = {
-    dateStart: '2017-01-01',
-    //today: Date.now(),
-    //month: Date.now(),
-    //timeStarts: Date.now(),
-    timeStart: '01:00',
-    dateEnd: '2017-01-02'
-  }
+	public event = {
+		dateStart: '2017-01-01',
+		//today: Date.now(),
+		//month: Date.now(),
+		//timeStarts: Date.now(),
+		timeStart: '01:00',
+		dateEnd: '2017-01-02'
+	}
 
-presentToast() {
-  let toast = this.toastCtrl.create({
-    message: 'Trip was added successfully',
-    duration: 3000,
-    position: 'top'
-  });
+	presentToast() {
+		let toast = this.toastCtrl.create({
+			message: 'Trip was added successfully',
+			duration: 3000,
+			position: 'top'
+		});
 
-  toast.onDidDismiss(() => {
-    console.log('Dismissed toast');
-  });
-  toast.present();
-}
+		toast.onDidDismiss(() => {
+			console.log('Dismissed toast');
+		});
+		toast.present();
+	}
 
-  
+	presentModal() {
+		console.log(this._friendsAdded);
+		let modal = this.modalCtrl.create(FriendsModal, {
+			currentUser: this._currentUser,
+			selectedFriends: this._friendsAdded
+		});
 
-  presentModal() {
-    let modal = this.modalCtrl.create(FriendsModal, {
-    currentUser: this._currentUser, selectedFriends: this._friendsAdded});
-
-    modal.onDidDismiss((setOfFriends) => {
+		modal.onDidDismiss((setOfFriends) => {
 			// setOfFriends.forEach((friend) => {
-      //   console.log(friend);
+			//   console.log(friend);
 			// 	this._friendsAdded.push(friend);
 			// });
-      console.log(this._friendsAdded);
-      
+			console.log(this._friendsAdded);
+
 		});
-    modal.present();
-  }
+		modal.present();
+	}
 
-  pushTrip() {
+	pushTrip() {
 
-    this._tripInfo = {
-      name: this.tripName,
-      location: this.tripLoc,
-      description: this.tripDescription,
-      transport: "car",
-      friends: this._friendsAdded,
-      items: this._itemList,
-      startTime: this.event.dateStart,
-      endTime: this.event.dateEnd,
-      leadOrganiser: this._currentUser.key,
-      coverPhotoUrl: "",
-      };
+		this._tripInfo = {
+			name: this.tripName,
+			location: this.tripLoc,
+			description: this.tripDescription,
+			transport: "car",
+			friends: this._friendsAdded,
+			items: this._itemList,
+			startTime: this.event.dateStart,
+			endTime: this.event.dateEnd,
+			leadOrganiser: this._currentUser.key,
+			coverPhotoUrl: "",
+		};
 
-      console.log(this._tripInfo);
-      if (this.tripName != "" && this.tripLoc != "" && this._friendsAdded.length != 0)
-      {
+		console.log(this._tripInfo);
+		if (this.tripName != "" && this.tripLoc != "" && this._friendsAdded.length != 0) {
 
-        console.log('success');
-        
-        this.firebasePush.pushNewTrip(this._tripInfo);  
-        this.clearTrip();
-        this.presentToast();
-    }
-  }
+			console.log('success');
 
-  clearTrip() {
-    this._tripInfo = [];
-    this.tripName = "";
-    this.tripLoc = "";
-    this.tripDescription = "";
-    this._friendsAdded = [];
-    this._itemList = [];
-    this.event = {
-      dateStart: '2017-01-01',
-      timeStart: '01:00',
-      dateEnd: '2017-01-02'
-    };
+			this.firebasePush.pushNewTrip(this._tripInfo);
+			this.clearTrip();
+			this.presentToast();
+		}
+	}
 
-  }
+	clearTrip() {
+		this._tripInfo = [];
+		this.tripName = "";
+		this.tripLoc = "";
+		this.tripDescription = "";
+		this._friendsAdded = [];
+		this._itemList = [];
+		this.event = {
+			dateStart: '2017-01-01',
+			timeStart: '01:00',
+			dateEnd: '2017-01-02'
+		};
 
-  addItem() {
-    if (this.title != "")
-    {
-      let newItem = {
-        name: this.title,
-        description: this.descr
-      };
-      this._itemList.push(newItem);
-      this.title = "";
-      this.descr = "";
-    }
+	}
 
-  } 
+	addItem() {
+		if (this.title != "") {
+			let newItem = {
+				name: this.title,
+				description: this.descr
+			};
+			this._itemList.push(newItem);
+			this.title = "";
+			this.descr = "";
+		}
 
-  deleteItem(item): void {
-    console.log(this._itemList);
-    let index = this._itemList.indexOf(item);
-    console.log(index);
-    this._itemList.splice(index, 1);
-    console.log(this._itemList);
-    
+	}
 
-  } 
-  
+	deleteItem(item): void {
+		console.log(this._itemList);
+		let index = this._itemList.indexOf(item);
+		console.log(index);
+		this._itemList.splice(index, 1);
+		console.log(this._itemList);
 
-  presentActionSheet() {
-    let actionSheet = this.actionSheetCtrl.create({
-      title: 'Edit Trip Picture',
-      buttons: [
-        {
-          text: 'Remove Trip Picture',
-          icon: !this.platform.is('ios') ? 'trash' : null,
-          role: 'destructive',
-          handler: () => {
-            console.log('Destructive clicked');
-          }
-        },{
-          text: 'Take Photo',
-          icon: !this.platform.is('ios') ? 'camera' : null,
-          handler: () => {
-            console.log('Take Photo clicked');
-          }
-        },{
-          text: 'Choose From Library',
-          icon: !this.platform.is('ios') ? 'folder-open' : null,
-          handler: () => {
-            console.log('Library clicked');
-          }
-        },{
-          text: 'Choose From Presets',
-          icon: !this.platform.is('ios') ? 'folder-open' : null,
-          handler: () => {
-            console.log('Presets clicked');
-          }
-        },{
-          text: 'Cancel',
-          icon: !this.platform.is('ios') ? 'close' : null,
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        }
-      ]
-    });
-    actionSheet.present();
-  }
-  
+	}
+
+	presentActionSheet() {
+		let actionSheet = this.actionSheetCtrl.create({
+			title: 'Edit Trip Picture',
+			buttons: [
+				{
+					text: 'Remove Trip Picture',
+					icon: !this.platform.is('ios') ? 'trash' : null,
+					role: 'destructive',
+					handler: () => {
+						console.log('Destructive clicked');
+					}
+				}, {
+					text: 'Take Photo',
+					icon: !this.platform.is('ios') ? 'camera' : null,
+					handler: () => {
+						console.log('Take Photo clicked');
+					}
+				}, {
+					text: 'Choose From Library',
+					icon: !this.platform.is('ios') ? 'folder-open' : null,
+					handler: () => {
+						console.log('Library clicked');
+					}
+				}, {
+					text: 'Choose From Presets',
+					icon: !this.platform.is('ios') ? 'folder-open' : null,
+					handler: () => {
+						console.log('Presets clicked');
+					}
+				}, {
+					text: 'Cancel',
+					icon: !this.platform.is('ios') ? 'close' : null,
+					role: 'cancel',
+					handler: () => {
+						console.log('Cancel clicked');
+					}
+				}
+			]
+		});
+		actionSheet.present();
+	}
+
 }
