b49c9384c3bffcd954bfde20b4bd6d15e5eb720d
George-PC\George <b3015660@my.shu.ac.uk>
2017-02-08 11:12:09 +0000

    Work on notifications

--- src/pages/notifications/notifications.ts
+++ src/pages/notifications/notifications.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, ModalController } from 'ionic-angular';
+import { AngularFire } from 'angularfire2';
 //import { FirebaseGET } from '../../services/firebase/get.service';
 //import { FirebasePUT } from "../../services/firebase/put.service";
 import { AuthenticationHandler } from "../../services/authenticationHandler.service";
@@ -14,23 +15,42 @@ export class Notifications {
   private _currentUser: any;  
   
   constructor(public navCtrl: NavController,
-//				public firebaseGet: FirebaseGET,
-//                public firebasePut: FirebasePUT,
 				public authenticationHandler: AuthenticationHandler,
-				public modalCtrl: ModalController
+				public modalCtrl: ModalController,
+                private af: AngularFire
 				) {
         this._notifications = [];
 
 		this._currentUser = this.authenticationHandler.getCurrentUser();
-        console.log(this._currentUser);
 
 		this._currentUser.notifications.forEach((notificationMessage) => {
-    
 				this._notifications.push(notificationMessage);
-    console.log(this._notifications)
 		});
 
   }
+    addNotification(): void {
+        this._currentUser = this.authenticationHandler.getCurrentUser();
+        const userNotifications = this.af.database.object("users/" + this._currentUser.key + "/notifications");
+        userNotifications.update({
+			notifications: "New Notification"
+		});
+//        console.log(this._currentUser.key);
+		
+	}
+    
+    	dismissNotification(notification): void {
+        this._currentUser = this.authenticationHandler.getCurrentUser();
+    console.log(notification);
+		const notificationObjectObservable = this.af.database.object("users/" + this._currentUser.key + "/notifications").remove();
+
+		notificationObjectObservable
+			.then(_ => console.log("Success!"))
+			.catch(err => console.log(err));
+	}
+    
+      	addText(): void {
+
+	}
 
 }
 
