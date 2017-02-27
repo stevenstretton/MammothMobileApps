e73c76ccbf1e108bfd842327bb4c013952cfbe21
George-PC\George <b3015660@my.shu.ac.uk>
2017-02-27 12:07:03 +0000

    Added a friend notification when you add a new person.

--- src/pages/friends/friends.ts
+++ src/pages/friends/friends.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, ModalController, ItemSliding } from 'ionic-angular';
 import { FirebaseGET } from '../../services/firebase/get.service';
 import { FirebasePUT } from "../../services/firebase/put.service";
+import { FirebasePOST } from "../../services/firebase/post.service";
 import { AuthenticationHandler } from "../../services/authenticationHandler.service";
 import { AddFriendModal } from "./modals/modals";
 import set = Reflect.set;
@@ -17,6 +18,7 @@ export class Friends {
 
 	constructor(public navCtrl: NavController,
 				public firebaseGet: FirebaseGET,
+				public firebasePost: FirebasePOST,
 				public authenticationHandler: AuthenticationHandler,
 				public modalCtrl: ModalController,
 				public firebasePut: FirebasePUT) {
@@ -58,6 +60,15 @@ export class Friends {
 
 	}
 
+		getNotifications(friendID): Array<any> {
+		let tempNotification;
+		this.firebaseGet.getUserWithID(friendID, (firebaseUser) => {
+			tempNotification = firebaseUser.notifications
+		})
+
+		return tempNotification;
+	}
+
 	presentAddFriendModal(): void {
 		let modal = this.modalCtrl.create(AddFriendModal, {
 			currentUser: this._currentUser
@@ -66,6 +77,20 @@ export class Friends {
 			if (setOfFriends) {
 				setOfFriends.forEach((friend) => {
 					this._friends.push(friend);
+					console.log(friend);
+					let usernotes = this.getNotifications(friend.key);
+
+				
+				if (usernotes == null) {
+					let usernotes = []
+					
+					usernotes.push(this._currentUser.firstName + " added you as a friend")
+					this.firebasePost.postNewNotification(friend.key, usernotes);
+				} else {
+					
+					usernotes.push(this._currentUser.firstName + " added you as friend")
+					this.firebasePut.putNewNotification(friend.key, usernotes);
+				}
 				});
 				this.firebasePut.putUserFriends(this._currentUser.key, this._friends);
 			}
@@ -74,3 +99,5 @@ export class Friends {
 		modal.present();
 	}
 }
+
+
